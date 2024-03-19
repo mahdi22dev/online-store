d66ac79edf64733443921d8fced850439da43df7
mahdi22dev <154707248+somerandomdev21@users.noreply.github.com>
2024-03-19 11:53:06 +0000

    chore: first commit

---
+++ src/app/home/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-24">
+      <h1 className="capitalize">
+        personal starter with next auth prisma shadcn ui
+      </h1>
+      <Button asChild variant={"default"}>
+        <Link href={"/login"}>Sign In</Link>
+      </Button>
+    </main>
+  );
+}
