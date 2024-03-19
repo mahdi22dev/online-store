2f4acd0a2d3890c20a2e75c6bce2aa1dabb96600
Mahdi idrissi <idrissimahdi2020@gmail.com>
2024-03-19 11:45:21 +0000

    first

---
+++ src/app/api/register/route.ts
@@ -0,0 +1,49 @@
+import { prisma } from "@/lib/prisma";
+import bcrypt from "bcrypt";
+import crypto from "crypto";
+import { NextApiRequest } from "next";
+import { NextResponse } from "next/server";
+
+export async function POST(req: NextApiRequest) {
+  // @ts-ignore
+  const data = await req.json();
+  const { name, email, password } = data.data;
+
+  if (!name || !email || !password) {
+    return NextResponse.json(
+      { message: "Correct the required data" },
+      { status: 400 }
+    );
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+  try {
+    const newUser = await prisma.user.create({
+      //@ts-ignore
+      data: {
+        userId: crypto.randomUUID(),
+        name: name,
+        email: email,
+        password: hashedPassword,
+      },
+    });
+
+    return NextResponse.json(
+      { message: "User Saved", newUser },
+      { status: 200 }
+    );
+  } catch (error) {
+    if (error.code === "P2002") {
+      return NextResponse.json(
+        { message: "This email already exist" },
+        { status: 409 }
+      );
+    } else {
+      return NextResponse.json(
+        { message: "Error saving user" },
+        { status: 400 }
+      );
+    }
+  } finally {
+    await prisma.$disconnect();
+  }
+}
