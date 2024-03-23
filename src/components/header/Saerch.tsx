41fa9d9f968bddacc6c98de079dc82b04ec52989
mahdi22dev <154707248+somerandomdev21@users.noreply.github.com>
2024-03-23 15:12:25 +0000

    add navbar

---
+++ src/components/header/Saerch.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { CiSearch } from "react-icons/ci";
+import { Input } from "../ui/input";
+function Saerch() {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <CiSearch className="text-3xl cursor-pointer" />
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <div className="relative w-full h-11">
+            <input
+              type="text"
+              className="w-full h-full border-none outline-none pl-6"
+              placeholder="What are your looking for?"
+            />
+            <CiSearch className="text-xl absolute top-3 left-0" />
+          </div>
+          <SheetDescription>Saerch results</SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
+}
+
+export default Saerch;
