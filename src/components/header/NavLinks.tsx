9b6c7693bd0aa15a56cf18e31e22f9b411c7aa16
mahdi22dev <154707248+somerandomdev21@users.noreply.github.com>
2024-03-24 00:48:59 +0000

    chore: fix nav

---
+++ src/components/header/NavLinks.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import React from "react";
+import { buttonVariants } from "../ui/button";
+import { cn } from "@/lib/utils";
+
+function NavLinks({ className }: { className?: string }) {
+  return (
+    <div
+      className={cn(
+        "flex gap-2 justify-between items-center capitalize",
+        className
+      )}
+    >
+      <Link href={"/home"} className={buttonVariants({ variant: "link" })}>
+        home
+      </Link>
+      <Link href={"/products"} className={buttonVariants({ variant: "link" })}>
+        products
+      </Link>
+      <Link href={"/devices"} className={buttonVariants({ variant: "link" })}>
+        devices
+      </Link>
+    </div>
+  );
+}
+
+export default NavLinks;
