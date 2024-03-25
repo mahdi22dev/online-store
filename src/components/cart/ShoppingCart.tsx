c26efd7518211b08d10bec2d2dea215791f75b0a
mahdi22dev <154707248+somerandomdev21@users.noreply.github.com>
2024-03-25 15:34:47 +0000

    redux cart slice

--- src/components/cart/ShoppingCart.tsx
+++ src/components/cart/ShoppingCart.tsx
@@ -6,51 +6,9 @@ function ShoppingCart() {
   return (
     <div className="bg-white rounded-sm  min-h-[384px] md:w-[450px] py-5 px-0 space-y-3 shadow hover:shadow-md focus:shadow-md duration-200">
       <div className="px-5 font-bold text-xl uppercase flex justify-start items-center gap-2">
-        <AiOutlineShopping /> <p>0 items</p>
+        <AiOutlineShopping /> <p>2 items</p>
       </div>
       <Separator />
-      <div className="px-5">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione porro
-        laboriosam, delectus, esse nam debitis vitae earum veniam itaque
-        reiciendis corrupti eos, sint nesciunt. Animi voluptatum natus quis
-        harum nostrum.
-      </div>
-      <div className="px-5">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione porro
-        laboriosam, delectus, esse nam debitis vitae earum veniam itaque
-        reiciendis corrupti eos, sint nesciunt. Animi voluptatum natus quis
-        harum nostrum.
-      </div>
-      <div className="px-5">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione porro
-        laboriosam, delectus, esse nam debitis vitae earum veniam itaque
-        reiciendis corrupti eos, sint nesciunt. Animi voluptatum natus quis
-        harum nostrum.
-      </div>{" "}
-      <div className="px-5">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione porro
-        laboriosam, delectus, esse nam debitis vitae earum veniam itaque
-        reiciendis corrupti eos, sint nesciunt. Animi voluptatum natus quis
-        harum nostrum.
-      </div>
-      <div className="px-5">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione porro
-        laboriosam, delectus, esse nam debitis vitae earum veniam itaque
-        reiciendis corrupti eos, sint nesciunt. Animi voluptatum natus quis
-        harum nostrum.
-      </div>
-      <div className="px-5">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione porro
-        laboriosam, delectus, esse nam debitis vitae earum veniam itaque
-        reiciendis corrupti eos, sint nesciunt. Animi voluptatum natus quis
-        harum nostrum.
-      </div>{" "}
-      <div className="px-5">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione porro
-        laboriosam, delectus, esse nam debitis vitae earum veniam itaque
-        reiciendis corrupti eos, sint nesciunt. Animi voluptatum natus quis
-        harum nostrum.
-      </div>
       <div className="px-5">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione porro
         laboriosam, delectus, esse nam debitis vitae earum veniam itaque
