7c593ea6e860f67be8be5d742e31ab0a450f48d9
mahdi22dev <idrissimahdi2020@gmail.com>
2024-06-02 00:36:18 +0100

    fix

--- src/app/info/privacy/page.tsx
+++ src/app/info/privacy/page.tsx
@@ -2,7 +2,7 @@ import SectionTitle from "@/components/text/SectionTitle";
 
 export default async function Privacy() {
   return (
-    <main className="min-h-screen pt-1">
+    <main className="min-h-screen pt-5">
       <SectionTitle text="Privacy" />
     </main>
   );
